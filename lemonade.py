2016f4d73ca8029660a23c5299a3aecc02c7aedd
0xabhishekchaudhary <125910874+Abhishek-Chaudhary28@users.noreply.github.com>
2025-08-14 23:20:30 +0530

    Lemon 2nd version

--- lemonade.py
+++ lemonade.py
@@ -7,10 +7,8 @@ new_day = input("Enter the No of the lemon for new day : ")
 sales_w2.append(int(new_day))
 sales = sales_w1 + sales_w2
 
-
-sales.sort()
-worstsales = sales[0] * 1.5
-bestsales = sales[-1] * 1.5
+worstsales = min(sales) * 1.5
+bestsales = max(sales) * 1.5
 
 
 print(f"Combined sales: {worstsales + bestsales}")
