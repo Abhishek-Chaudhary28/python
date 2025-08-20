0b1a1221effed75b1dd4411a6d21689604b10130
0xabhishekchaudhary <125910874+Abhishek-Chaudhary28@users.noreply.github.com>
2025-08-20 10:16:39 +0530

    new

---
+++ Evaluate/new.js
@@ -0,0 +1,13 @@
+// function abc() {
+//  console/log(abc.xyz) // This function is intentionally left empty
+// }
+
+// abc()
+// abc.xyz = 400
+// abc.xyz =200
+// abc()
+
+
+const nmbers = [1, 2, 3, 4];
+nmbers[100] = 500;
+console.log(nmbers);
